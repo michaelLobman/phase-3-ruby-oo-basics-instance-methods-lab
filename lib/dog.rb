ef611b3b01de2cc64cc7c46667b52b881b47d173
Michael Lobman <michael.lobman@gmail.com>
2022-01-12 11:47:03 -0500

    Complete assignment

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
+
+murphy = Dog.new
+
+murphy.bark
+murphy.sit
+
+snoopy = Dog.new
+snoopy.bark
